32c043bdd4b0d5880871963362fe6c7640597591
webdateng <webdateng@gmail.com>
2021-12-26 20:23:09 +0800

    first commit

---
+++ js/sound.js
@@ -0,0 +1,32 @@
+var audio = new Audio();
+// GANTI LAGU
+audio.src = "sound/sound.mp3";
+// LOOP
+audio.loop = true;
+
+function initAudioPlayer() {
+  var playbtn = document.getElementById("playpausebtn");
+  var playbtn2 = document.getElementById("playbtn");
+  var myPopup = document.getElementById("popup");
+
+  playbtn.addEventListener("click", playPause);
+
+  function playPause() {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+  playbtn2.addEventListener("click", playPause);
+
+  function playPause() {
+    if (audio.paused) {
+      audio.play();
+      myPopup.style.display = "none";
+    } else {
+      audio.pause();
+    }
+  }
+}
+window.addEventListener("load", initAudioPlayer);
